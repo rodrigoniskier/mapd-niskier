c1714f550fbf246b28cad758ee51a11e22454755
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-08-06 23:51:58 -0300

    feat: aplica padrão ENAMED no comando de geração

--- core/management/commands/gerar_todos_pacotes.py
+++ core/management/commands/gerar_todos_pacotes.py
@@ -47,14 +47,14 @@ class Command(BaseCommand):
                         Questao.objects.create(
                             tema=tema,
                             tipo=Questao.Tipo.OBJETIVA,
-                            enunciado=q.enunciado,
+                            enunciado=q.enunciado_completo(),
                             alternativas=q.alternativas_dict(),
                             gabarito=q.gabarito,
                             justificativa=q.justificativa,
                             dificuldade=q.dificuldade,
                             nivel_cognitivo=q.nivel_cognitivo,
-                            habilidade=q.habilidade,
-                            referencias=[f.model_dump() for f in q.referencias],
+                            habilidade=q.habilidade[:200],
+                            referencias=q.referencias_registro(),
                             criada_por_ia=True,
                         )
                     for q in pacote.discursivas[:2]:
